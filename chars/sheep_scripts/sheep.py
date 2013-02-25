e80ab3059723ba2fa8156645686ac44e32e14ee0
Andrew Reisner <andrew@reisner.co>
2013-02-25 11:58:37 -0600

    new sheep api

--- chars/sheep_scripts/sheep.py
+++ chars/sheep_scripts/sheep.py
@@ -9,25 +9,45 @@ import bge
 class Sheep(bge.types.KX_GameObject):
     
     def __init__(self,old_owner):
-        self.animations = {}
-        pass
+        self.status = None
+        self.actmap = {
+                       'walk':'sheep_walk',
+                       'run':'sheepcute_run',
+                       'graze':'sheep_graze',
+                       'helpless':'sheepcute_helpless',
+                       'hit':'hit_sheep',
+                       'flounder': 'sheep_flounder',
+                       'puff':'sheep_puff',
+                       'drown':'sheep_drown',
+                       'motion':'sheep_motion'
+                       }
+        
+    def act(self,name):
+        return self.controller.actuators[self.actmap[name]]
     
-    def registerAnimation(self,name,controller,actuator):
-        self.animations[name] = (controller,actuator)
+    def walk(self,speed=.01):
+        speed *= -1
+        if self.status != None:
+            self.controller.deactivate(self.act(self.status))
+        self.status = 'walk'
+        
+        self.controller.activate(self.act('walk'))
+        self.act('motion').dLoc = (0,speed,0)
     
-    def animate(self,action):
-        if action in self.animations.keys():
-            self.animations[action][0].activate(self.animations[action][1])
+    def turn(self,angle):
+        self.act('motion').dRot = (0,0,angle)
+        
+    def update(self):
+        self.controller.activate(self.act('motion'))
+        print(self.worldPosition)
+        
     
+        
 def init(cont):
-    sheep = Sheep(cont.owner)
-    acts = cont.actuators
-    sheep.registerAnimation('walk', cont,acts['sheep_walk'])
-    sheep.registerAnimation('run', cont,acts['sheepcute_run'])
-    sheep.registerAnimation('graze',cont,acts['sheep_graze'])
-    sheep.registerAnimation('helpless',cont,acts['sheepcute_helpless'])
-    sheep.registerAnimation('hit',cont,acts['hit_sheep'])
-    sheep.registerAnimation('flounder',cont,acts['sheep_flounder'])
-    sheep.registerAnimation('puff',cont,acts['sheep_puff'])
-    sheep.registerAnimation('drown',cont,acts['sheep_drown'])
-    sheep.animate('run')
+    if not cont.owner['initialized']:
+        sheep = Sheep(cont.owner)
+        sheep.controller = cont
+        sheep.walk()
+        cont.owner['initialized'] = True
+    else:
+        cont.owner.update()
